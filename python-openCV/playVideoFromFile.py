a7514a28109884c186cbcd49dba453fbe720c2c9
Sana Asif <sana98asif@gmail.com>
2023-03-17 23:46:58 +0530

    My repo.

--- python-openCV/playVideoFromFile.py
+++ python-openCV/playVideoFromFile.py
@@ -1,10 +1,12 @@
 import cv2
 
 cap = cv2.VideoCapture("Capture_Video.mp4")
-while(True):
-    ret_,frame = cap.read()
-    cv2.imshow("frame",frame)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+while True:
+    isTrue,frame = cap.read()
+    cv2.imshow("video",frame)
+
+    if cv2.waitKey(20) & 0xFF == ord("q"):
         break
+
 cap.release()
 cv2.destroyAllWindows()
