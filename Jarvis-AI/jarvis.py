6eaffb2c25442eda168bb19caa8ccdb8f552dcd9
Sana Asif <sana98asif@gmail.com>
2023-05-23 23:30:50 +0530

    My repo.

--- Jarvis-AI/jarvis.py
+++ Jarvis-AI/jarvis.py
@@ -1,6 +1,8 @@
 import pyttsx3
 import datetime
 import speech_recognition as sr
+import wikipedia
+import webbrowser
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -21,6 +23,41 @@ def wishMe():
         speak("Good Evening")
 
     speak("Myself Elsa how may I help you?")
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio,language='en-in')
+        print(f"User said: {query}\n")
+    except Exception as e:
+        print("Say that again please...")
+        return "None"
+    return query
+
 if __name__ == "__main__":
     speak("Hello Ma'am!")
     wishMe()
+    while(True):
+        query = takeCommand().lower()
+        if 'wikipedia' in query:
+            speak("Searching wikipedia...")
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query,sentences=2)
+            speak("According to wikipedia")
+            print(results)
+            speak(results)
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+        elif 'open geek for geeks' in query:
+            webbrowser.open("geekforgeeks.org")
+        elif 'open udemy' in query:
+            webbrowser.open("udemy.com")
+        elif 'open take u forward' in query:
+            webbrowser.open("takeuforward.org")
+        
